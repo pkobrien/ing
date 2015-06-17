aad384f112c6d9f65f1d340eee554ae73f4effdb
Patrick K. O'Brien <patrick.keith.obrien@gmail.com>
2015-06-16 21:42:59 -0500

    Keep renaming stuff until we get it just right

--- strap/src/app/core.cljs
+++ strap/src/app/core.cljs
@@ -11,7 +11,9 @@
 
 (enable-console-print!)
 
-(defonce init-state {:title "Something"})
+(defonce init-state
+  {:title "Something"
+   :heading "Demonstrating Something"})
 
 (defonce app-state (atom init-state))
 
@@ -19,18 +21,44 @@
   (set! (. js/document -title) title)
   js/document.title)
 
-(defn body []
-  (html [:body
-         [:h1 "Demonstrating Something"]
-         [:p {:style {:color "red"}} "Hello, World!"]
-         ]))
+#_(defn app-html [data]
+  (html
+   [:body
+    [:header]
+    [:aside]
+    [:main]
+    [:footer]
+    ]))
 
-(defn root [data]
-  (om/component
-   (body)))
+(defn app-html [data]
+  (html
+   [:body
+    [:header
+     [:h1 (:heading data)]
+     ]
+    [:aside
+     [:p {:style {:color "red"}} "This is within an aside..."]
+     [:details
+      [:summary "Click here for more details..."]
+      [:p "Om is " [:a {:href "https://github.com/omcljs/om"} "available on GitHub"]]
+      ]
+     ]
+    [:main
+     [:p {:style {:color "green"}} "Hello, World!"]
+     ]
+    [:footer
+     [:p "This is within the footer."]
+     ]
+    ]))
+
+(defn app-root [data owner]
+  (om/component (app-html data)))
 
 (defn main []
-  (om/root root app-state {:target js/document.body}))
+  (om/root app-root app-state {:target js/document.body}))
+
+#_(defn main []
+  (om/root app-root app-state {:target (. js/document (getElementById "app"))}))
 
 (defn init []
   (set-title (:title init-state))
